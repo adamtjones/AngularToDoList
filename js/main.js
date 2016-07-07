d67fef1b21536a91356527da4ac178f38943a55e
Adam Jones <adamjns802@gmail.com>
2016-07-07 16:55:37 -0400

    first push of AngularToDoList

---
+++ js/main.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+    angular
+        .module('inventory', [])
+        .controller('HeaderController', function Header() {
+            if (localStorage.getItem("todos")!==null) {
+            	this.data = JSON.parse(localStorage.getItem("todos"));
+           } else {
+            	this.data = [];
+            };
+
+		this.submit = function (isValid) {
+		var newObj = {"name": this.form.name, /*"isComplete"=false*/};
+		
+		if (isValid) {
+			this.data.push(newObj);
+			localStorage.setItem('todos', JSON.stringify(this.data));
+			this.form = [];
+	}
+	
+	};
+
+	/*var itemsLeft =	data.length
+
+	$(".itemsLeft").append(itemsLeft+'items left')*/
+
+
+})})();
+
+
